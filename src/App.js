64191d9aae4ed49539aa28287dd45d137ce4bc6e
Reese <reese.crockett@gmail.com>
2019-09-05 12:04:55 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReverseList from './ReverseList';
+import List from './List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id='topBanner'>Reverse String</div>
+      <div id='inputBar'>
+        <input id='txt' placeholder='Reverse here' type='text' />
+        <button id='btn'>Submit</button>
+      </div>
+        <List />
+        <ReverseList />
+      </div>
   );
 }
 
---
+++ src/ReverseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReverseList = () => {
+    return (
+        <div className='Container'>
+            <h1>List of Reversed Text</h1>
+            <ul></ul>
+        </div>
+    )
+}
+
+export default ReverseList;
